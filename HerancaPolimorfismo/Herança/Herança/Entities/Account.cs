c730ca415a21a8ac4ce2ab75e5a37587b3b95d1e
Aencio <aenpvp9@gmail.com>
2024-06-24 15:01:25 -0300

    feature: file added HerancaPolimorfismo

---
+++ HerancaPolimorfismo/Herança/Herança/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Course.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ HerancaPolimorfismo/Herança/Herança/Program.cs
@@ -0,0 +1,15 @@
+﻿using Course.Entities;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            businessAccount account = new businessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
