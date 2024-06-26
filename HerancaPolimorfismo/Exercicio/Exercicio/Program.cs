642cc54cad59f94ebf2cc3fd867e1f85bbef367f
Aencio <aenpvp9@gmail.com>
2024-06-26 16:41:04 -0300

    exercice finalized

--- HerancaPolimorfismo/Exercicio/Exercicio/Program.cs
+++ HerancaPolimorfismo/Exercicio/Exercicio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using Course.Entities;
 using Exercicio.Entities;
@@ -18,6 +19,31 @@ namespace Course
             {
                 Console.Write($"Employee number #{i} data:");
                 Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addiTionalCharge = double.Parse((string) Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, addiTionalCharge));
+                } else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PAYMENTS: ");
+                
+                foreach(Employee emp in list)
+                {
+                    Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
             }
 
         }
