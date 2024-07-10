03581a78f2fa4d08c8ee5b1ad470be7739efbd2a
Aencio <aenpvp9@gmail.com>
2024-07-10 14:52:50 -0300

    feat: daily commit

---
+++ tratamentoDeExecoes/criandoExcecoes/criandoExcecoes/Program.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities;
+using Course.Entities.Exceptions;
+using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            try { 
+                Console.WriteLine("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Check-in date (dd/MM/YYYY): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Check-out date (dd/MM/YYYY): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation");
+                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.updateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e) {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e) {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
