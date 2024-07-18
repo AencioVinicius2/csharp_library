9c1de9256202dd89156f47ea13c73d29d70c047e
Aencio <aenpvp9@gmail.com>
2024-07-18 15:41:40 -0300

    daily commit

---
+++ arquivos/File, FileInfo e IOException/File, FileInfo e IOException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Course { 
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\file1.txt";
+            string targetPath = @"c:\file2.txt";
+
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            } 
+            catch (Exception e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
