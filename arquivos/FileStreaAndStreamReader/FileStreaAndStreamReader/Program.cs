1c917139bef80c3655741b3a9cf10118b9f04bc3
Aencio <aenpvp9@gmail.com>
2024-07-22 11:19:14 -0300

    daily commit

---
+++ arquivos/FileStreaAndStreamReader/FileStreaAndStreamReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:C:\Users\aenpv\OneDrive\Área de Trabalho\file1.txt";
+            StreamReader sr = null;
+
+            try {
+                sr = new StreamReader(path);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            } catch (IOException e) {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            } 
+            finally {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
