de321ad22e86175d141bc75864282f80136bfdc5
Aencio <aenpvp9@gmail.com>
2024-07-25 13:53:22 -0300

    feat: daily commit

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO ;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ path/path/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\temp\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator) ;
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); ;
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempoPath: " + Path.GetTempPath());
+        }
+    }
+}
