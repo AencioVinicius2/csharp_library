c6859c7692c7b44dd9161138887dddc404c59053
Aencio <aenpvp9@gmail.com>
2024-07-30 11:46:07 -0300

    daily commit

---
+++ Interfaces/Interfaces/Interfaces/Entities/CarRental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Interfaces.Entities {
+    class CarRental {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle) {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Interfaces/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfaces.Entities
+{
+    class Vehicle {
+        public string Model { get; set; }
+
+        public Vehicle(string model) {
+            Model = model;
+        }
+    }
+}
