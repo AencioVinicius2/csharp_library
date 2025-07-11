bca05c42c9352204c5fdc5e398424698ec835143
Aencio <aenpvp9@gmail.com>
2025-07-11 10:50:53 -0300

    feat: melhorando MembrosEstaticos

--- revendo/MembrosEstaticos/MembrosEstaticos/Program.cs
+++ revendo/MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -5,30 +5,15 @@ namespace MembrosEstaticos
 {
     class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
-        {
+        {   
 
-            Console.WriteLine("Entre o valor do raio: ");
+            Console.Write("Entre o valor do raio: ");
+
             double raio = double.Parse(Console.ReadLine());
 
- 
-            double circ = Circunferencia(raio);
-            double voçume = Voume(raio);
-
-
-            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Volume: " + voçume.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circunferencia(double raio)
-        {
-            return 2 * Pi * raio;
-        }
-
-        static double Voume (double raio)
-        {
-            return 4.0 / 3.0 * Pi * raio;
+            Console.WriteLine("Circunferência: " + Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
