659381be2ade52bc7d485f8064fd17b3e3b0d975
Aencio <aenpvp9@gmail.com>
2025-07-11 10:08:08 -0300

    add: delegacao

---
+++ revendo/Delegacao/Delegacao/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverQuantidade(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                +  Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                +", " 
+                + Quantidade
+                + " unidades, Total: $ " 
+                + (Preco * Quantidade).ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
