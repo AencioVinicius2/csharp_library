8dc1cafa73cac3052268e3a46e387f48e92edd34
Aencio <aenpvp9@gmail.com>
2025-07-11 13:47:53 -0300

    add: Encapsulamento

---
+++ revendo/Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverQuantidade(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if(nome != null && nome.Length > 1 )
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            if(preco != 0)
+            {
+                _preco = preco;
+            }
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade)
+        {
+            if (quantidade != 0)
+            {
+                _quantidade = quantidade;
+            }
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + (_preco * _quantidade).ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
