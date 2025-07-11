626660afb69e270000d43d70f60e57b670c73cf1
Aencio <aenpvp9@gmail.com>
2025-07-11 11:35:51 -0300

    add: exercicio1 ConversorDeMoedas

---
+++ revendo/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolares, cotDoll;
+            Console.Write("Qual a cotação do dolar? ");
+            cotDoll = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            dolares = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoedas.CalculaConversao(dolares, cotDoll).ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
