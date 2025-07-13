a9176dc2218cad7b2c5b027793dd53d2dddbcc72
Aencio <aenpvp9@gmail.com>
2025-07-13 20:03:03 -0300

    add: ExercicioVetores

---
+++ revendo/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nQuartos = 0;
+
+            Quarto[] quartos = new Quarto[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+
+            nQuartos = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < nQuartos; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i+1).ToString());
+                Console.Write("Nome: "); 
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                quartos[i] = new Quarto { Nome = nome, Email = email, QuartoId = quarto };
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+
+            for(int i = 0; i < nQuartos; i++)
+            {
+                if (!(quartos[i].QuartoId is Nullable))
+                {
+                    Console.WriteLine((i + 1).ToString() + ": " + quartos[i].Nome + ", " + quartos[i].Email);
+                }
+            }
+            
+
+
+        }
+    }
+}
---
+++ revendo/ConsoleApp3/ConsoleApp3/Quarto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioVetores
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int QuartoId {  get; set; }
+
+
+    }
+}
