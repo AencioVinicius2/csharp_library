68badff78452af2a202c8196c0b54d200dc12ac3
Aencio <aenpvp9@gmail.com>
2025-07-13 20:19:44 -0300

    add: funcao Sum sem o params

---
+++ revendo/ModificadorParams/ModificadorParams/Program.cs
@@ -0,0 +1,14 @@
+﻿using ModificadorParams;
+using System;
+
+namespace ModificdorParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(new int[] {1, 2, 3});
+            int s2 = Calculator.Sum(new int[] { 1, 2, 3, 4 });
+        }
+    }
+}
