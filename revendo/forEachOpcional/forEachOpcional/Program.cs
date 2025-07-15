6c81869a2dbce860bd00d4dd496f013481f9d31d
AencioVincius2 <aenciosilva@gmail.com>
2025-07-14 21:38:33 -0300

    add: forEach

---
+++ revendo/forEachOpcional/forEachOpcional/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace forEachOpcional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+            
+            foreach(string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+         
+        }
+    }
+
+}
