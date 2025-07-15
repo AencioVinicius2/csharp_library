022b65ba640c35a86fa3e83f27dd670d4fae6caa
Aencio <aenpvp9@gmail.com>
2025-07-15 11:14:51 -0300

    add: listas

---
+++ revendo/lists/lists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() {"Maria", "Bob", "Marley"};
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            string s1 = list.Find(x => x[0] == 'A');
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+
+
+        }
+    }
+}
