d6a11a13c9394d7d11748a7e24f3875094d59b23
AencioVincius2 <aenciosilva@gmail.com>
2025-07-14 21:34:37 -0300

    add: boxing e unboxing

---
+++ revendo/boxEunboxing/boxEunboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace boxANDunboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing, é o processo de conversão de um objeto tipo valor para um objeto tipo referencia compativel
+            int x = 20; // [X] stack
+            Object obj = x; // obj é uma referencia para um objeto no heap (custa mais processamento e memoria)
+
+            // é o processo do conversão de um objeto tipo referência para um objeto tipo valor compátivel
+            int y = (int) obj; // variavel y é criada no stack e recebe o valor [20], que estava dentro do heap (variavel x) [20]
+
+
+        }
+    }
+        
+}
