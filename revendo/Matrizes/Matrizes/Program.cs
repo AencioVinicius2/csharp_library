d4e566677e4e9f647c0677563af6e56f093a9f25
Aencio <aenpvp9@gmail.com>
2025-07-21 13:57:18 -0300

    add: exercicio Matriz part1

---
+++ revendo/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Input: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[,] mat = new double[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+
+
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
