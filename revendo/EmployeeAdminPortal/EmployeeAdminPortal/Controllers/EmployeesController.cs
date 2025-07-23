ec6d9f276a5a593e855e44d3ae8bf3e242fc9d0f
Aencio <aenpvp9@gmail.com>
2025-07-23 16:32:52 -0300

    feat: finish employee controller

--- revendo/EmployeeAdminPortal/EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ revendo/EmployeeAdminPortal/EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeAdminPortal.Data;
 
 namespace EmployeeAdminPortal.Controllers
 {
 
-    //localhost:xxxx/apo/employees
+    //localhost:xxxx/apo/employees  
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+
+
+        [HttpGet] 
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
     }
 }
