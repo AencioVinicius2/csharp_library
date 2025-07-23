8044ebacf8cb750b860652a7b01faf7be4103ed9
Aencio <aenpvp9@gmail.com>
2025-07-23 15:34:01 -0300

    add: Employee crud

---
+++ revendo/EmployeeAdminPortal/EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+            
+        }
+    }
+}
---
+++ revendo/EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }   
+        public decimal Salary { get; set; }
+
+
+    }
+}
