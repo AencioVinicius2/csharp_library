010e4f0c3a4385b4c62aa3577f9ce5d2d0100ccf
Aencio <aenpvp9@gmail.com>
2025-08-04 14:02:44 -0300

    add: post method in StudentsController

--- MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
+++ MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
 
 namespace StudentPortal.Web.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -11,9 +21,23 @@ namespace StudentPortal.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add()
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
         {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                PhoneNumber = viewModel.PhoneNumber,
+                Subscribed = viewModel.Subscribed
+            };
 
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+
+
+
+            return View();
         }
     }
 }
--- MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Models/AddStudentViewModel.cs
+++ MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace StudentPortal.Web.Models
 {
     public class AddStudentViewModel
-    {
+    {   
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Subscribed { get; set; }
     }
 }
