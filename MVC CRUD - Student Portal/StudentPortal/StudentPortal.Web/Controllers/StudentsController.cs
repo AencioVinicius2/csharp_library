1a233363372871b36db44e9def66bce57dc9e72e
Aencio <aenpvp9@gmail.com>
2025-08-04 17:12:48 -0300

    add: Edit method

--- MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
+++ MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
@@ -43,6 +43,36 @@ namespace StudentPortal.Web.Controllers
         {                                           
             var students = await dbContext.Students.ToListAsync();
 
+
+            return View(students);
+
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if(student != null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.PhoneNumber = viewModel.PhoneNumber;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+
+            }
+            return RedirectToAction("List","Students");
+        }
+
     }
 }
