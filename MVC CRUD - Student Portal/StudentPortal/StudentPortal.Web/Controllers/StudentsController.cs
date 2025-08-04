bfd1976413891172e4238764fba67c1463ae765d
Aencio <aenpvp9@gmail.com>
2025-08-04 17:30:54 -0300

    add: delete method

--- MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
+++ MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
@@ -73,6 +73,17 @@ namespace StudentPortal.Web.Controllers
             }
             return RedirectToAction("List","Students");
         }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == viewModel.Id);
 
+            if(student != null)
+            {
+                dbContext.Students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
     }
 }
