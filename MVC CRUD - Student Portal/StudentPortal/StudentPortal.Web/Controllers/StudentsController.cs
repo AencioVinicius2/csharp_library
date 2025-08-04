8071d3f9c4924f29b86f13fa02e45e0a1eed6c0d
Aencio <aenpvp9@gmail.com>
2025-08-04 14:21:45 -0300

    add: List view

--- MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
+++ MVC CRUD - Student Portal/StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentPortal.Web.Data;
 using StudentPortal.Web.Models;
 using StudentPortal.Web.Models.Entities;
@@ -34,10 +35,14 @@ namespace StudentPortal.Web.Controllers
             await dbContext.Students.AddAsync(student);
             await dbContext.SaveChangesAsync();
 
-
-
-
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {                                           
+            var students = await dbContext.Students.ToListAsync();
+
         }
     }
 }
