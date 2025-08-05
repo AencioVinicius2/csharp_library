84a4873c11d5a139dae52e016a7def6d95b43379
Aencio <aenpvp9@gmail.com>
2025-08-05 19:07:06 -0300

    add: AddContact And GetAllContacts methods

---
+++ revendo/Contact-app/API/Contactly/Contactly/Controllers/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿using Contactly.Data;
+using Contactly.Models;
+using Contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+        public ContactsController(ContactlyDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContacts()
+        {
+            var contacts = await dbContext.Contacts.ToListAsync();
+
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite
+            };
+            await dbContext.Contacts.AddAsync(domainModelContact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(request);
+        }
+
+    }
+}
---
+++ revendo/Contact-app/API/Contactly/Contactly/Models/AddRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Contactly.Models
+{
+    public class AddContactRequestDTO
+    {
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public required string Phone { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
