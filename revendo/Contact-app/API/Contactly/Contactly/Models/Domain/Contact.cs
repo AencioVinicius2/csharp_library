398a8d454952b3d5e6e1f27a1ef7d707b494003f
Aencio <aenpvp9@gmail.com>
2025-08-05 11:25:59 -0300

    add: Models -> Domain -> Contact class

---
+++ revendo/Contact-app/API/Contactly/Contactly/Models/Domain/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Contactly.Models.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public required string Phone { get; set;}
+        public bool Favorite { get; set; }
+    }
+}
