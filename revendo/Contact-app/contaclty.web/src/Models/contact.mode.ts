258fcb30dcd0643213ee711a1fa23416c10d4564
Aencio <aenpvp9@gmail.com>
2025-08-06 15:01:42 -0300

    feat: before updated angular cli

---
+++ revendo/Contact-app/contaclty.web/src/Models/contact.mode.ts
@@ -0,0 +1,7 @@
+export interface Contact {
+    id: string;
+    name: string;
+    email: string | null;
+    phone: string;
+    favorite: boolean;
+}
--- revendo/Contact-app/contaclty.web/src/app/app.component.ts
+++ revendo/Contact-app/contaclty.web/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from '../Models/contact.mode';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contaclty.web';
+  http = inject(HttpClient);
+
+  contacts$ = this.getContacts();
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7182/Api/Contacts');
+  }
 }
