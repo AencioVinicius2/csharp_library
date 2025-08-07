5a23ee44a79dff76b01251e62bc02b79b46a13e4
Aencio <aenpvp9@gmail.com>
2025-08-07 12:08:10 -0300

    feat: add icons and new input/output bindings to AppComponent

--- revendo/Contact-app/contaclty.web/src/app/app.component.ts
+++ revendo/Contact-app/contaclty.web/src/app/app.component.ts
@@ -4,18 +4,31 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient, HttpClientModule  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Contact } from '../Models/contact.mode';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, NgIf, HttpClientModule, NgFor ],
+  imports: [RouterOutlet, AsyncPipe, NgIf, HttpClientModule, NgFor, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   http = inject(HttpClient);
 
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string | null>(null),
+    favorite: new FormControl<boolean>(false)
+  })
+
   contacts$ = this.getContacts();
+
+  onFormSubmit() {
+    console.log(this.contactsForm.value);
+  }
 
   private getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>('https://localhost:7182/api/Contacts');
